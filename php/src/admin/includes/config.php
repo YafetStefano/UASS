9ba72f83181705573e07318e8d9a67beca76a11a
YafetStefano <yafetstefano.ys10@gmail.com>
2021-12-30 03:00:33 +0700

    UASS

---
+++ php/src/admin/includes/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','10.0.0.45');
+define('DB_USER','admin');
+define('DB_PASS' ,'Enambelas16-');
+define('DB_NAME','newsportal');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
